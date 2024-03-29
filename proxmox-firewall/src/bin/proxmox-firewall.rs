924905955ace4a2aa6f870efe10eb27a890800b4
Stefan Hanreich <s.hanreich@proxmox.com>
2024-03-29 15:29:15 +0100

    firewall: add proxmox-firewall binary and move existing code into lib
    Co-authored-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
    Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Reviewed-by: Lukas Wagner <l.wagner@proxmox.com>
    Reviewed-by: Max Carrara <m.carrara@proxmox.com>

---
+++ proxmox-firewall/src/bin/proxmox-firewall.rs
@@ -0,0 +1,96 @@
+use std::io::Write;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use anyhow::{Context, Error};
+
+use proxmox_firewall::firewall::Firewall;
+use proxmox_nftables::{client::NftError, NftClient};
+
+const RULE_BASE: &str = include_str!("../../resources/proxmox-firewall.nft");
+
+fn remove_firewall() -> Result<(), std::io::Error> {
+    log::info!("removing existing firewall rules");
+    let commands = Firewall::remove_commands();
+
+    // can ignore other errors, since it fails when tables do not exist
+    if let Err(NftError::Io(err)) = NftClient::run_json_commands(&commands) {
+        return Err(err);
+    }
+
+    Ok(())
+}
+
+fn handle_firewall() -> Result<(), Error> {
+    let firewall = Firewall::new();
+
+    if !firewall.is_enabled() {
+        return remove_firewall().with_context(|| "could not remove firewall tables".to_string());
+    }
+
+    log::info!("creating the firewall skeleton");
+    NftClient::run_commands(RULE_BASE)?;
+
+    let commands = firewall.full_host_fw()?;
+
+    log::info!("Running proxmox-firewall commands");
+    for (idx, c) in commands.iter().enumerate() {
+        log::debug!("cmd #{idx} {}", serde_json::to_string(&c)?);
+    }
+
+    let response = NftClient::run_json_commands(&commands)?;
+
+    if let Some(output) = response {
+        log::debug!("got response from nftables: {output:?}");
+    }
+
+    Ok(())
+}
+
+fn init_logger() {
+    match std::env::var("RUST_LOG_STYLE") {
+        Ok(s) if s == "SYSTEMD" => env_logger::builder()
+            .format(|buf, record| {
+                writeln!(
+                    buf,
+                    "<{}>{}: {}",
+                    match record.level() {
+                        log::Level::Error => 3,
+                        log::Level::Warn => 4,
+                        log::Level::Info => 6,
+                        log::Level::Debug => 7,
+                        log::Level::Trace => 7,
+                    },
+                    record.target(),
+                    record.args()
+                )
+            })
+            .init(),
+        _ => env_logger::init(),
+    };
+}
+
+fn main() -> Result<(), std::io::Error> {
+    init_logger();
+
+    let term = Arc::new(AtomicBool::new(false));
+
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
+    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))?;
+
+    while !term.load(Ordering::Relaxed) {
+        let start = Instant::now();
+
+        if let Err(error) = handle_firewall() {
+            log::error!("error creating firewall rules: {error}");
+        }
+
+        let duration = start.elapsed();
+        log::info!("firewall update time: {}ms", duration.as_millis());
+
+        std::thread::sleep(Duration::from_secs(5));
+    }
+
+    remove_firewall()
+}
---
+++ proxmox-firewall/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod config;
+pub mod firewall;
+pub mod object;
+pub mod rule;

7acba538b8243394c196d127b50bf385c671ea79
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-04-26 16:02:38 +0200

    service: add flag file to force-disable nftables
    This will be set in the iptables/Perl based pve-firewall service and
    ensures that users pulling this service but not enabling it are
    "protected" from log-spam due to some pve-firewall config not being
    parsed correctly (yet).
    
    We can remove this again once we're sure that all edge cases are
    covered, or this service here becomes the default.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
    Tested-by: Stefan Hanreich <s.hanreich@proxmox.com>

--- proxmox-firewall/src/bin/proxmox-firewall.rs
+++ proxmox-firewall/src/bin/proxmox-firewall.rs
@@ -11,6 +11,8 @@ use proxmox_nftables::{client::NftError, NftClient};
 
 const RULE_BASE: &str = include_str!("../../resources/proxmox-firewall.nft");
 
+const FORCE_DISABLE_FLAG_FILE: &str = "/run/proxmox-nftables-firewall-force-disable";
+
 fn remove_firewall() -> Result<(), std::io::Error> {
     log::info!("removing existing firewall rules");
 
@@ -83,7 +85,13 @@ fn main() -> Result<(), std::io::Error> {
     signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
     signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))?;
 
+    let force_disable_flag = std::path::Path::new(FORCE_DISABLE_FLAG_FILE);
+
     while !term.load(Ordering::Relaxed) {
+        if force_disable_flag.exists() {
+            std::thread::sleep(Duration::from_secs(5));
+            continue;
+        }
         let start = Instant::now();
 
         if let Err(error) = handle_firewall() {

proxmox/proxmox-firewall

commit 1a91751740b8ced17324e84e696fc5b9d9b8f034

fix #6107: add support for legacy ipset / alias names
Change the existing firewall rule generation logic to accomodate the
changes in the config parser, so it can deal with the legacy ipset /
alias names.

The lookup logic is the same as in pve-firewall: In guest context,
check if it is contained in the guest config, otherwise check the
cluster config. In cluster context, only check the cluster
configuration.

Because the rule generation logic now has to look up ipsets instead of
blindly inserting rules with the ipset name, store the generated SDN
ipsets in the firewall config as well, to avoid re-generating them for
every lookup. This should also allow for better error reporting, in
case ipsets or aliases are referenced that do not exist in the
configuration.

Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20250925122403.230867-4-s.hanreich@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-firewall/src/config.rs
@@ -11,8 +11,10 @@ use proxmox_ve_config::firewall::bridge::Config as BridgeConfig;
 use proxmox_ve_config::firewall::cluster::Config as ClusterConfig;
 use proxmox_ve_config::firewall::guest::Config as GuestConfig;
 use proxmox_ve_config::firewall::host::Config as HostConfig;
-use proxmox_ve_config::firewall::types::alias::{Alias, AliasName, AliasScope};
+use proxmox_ve_config::firewall::types::alias::{Alias, AliasScope, RuleAliasName};
 
+use proxmox_ve_config::firewall::types::ipset::{IpsetScope, RuleIpsetName};
+use proxmox_ve_config::firewall::types::Ipset;
 use proxmox_ve_config::guest::types::Vmid;
 use proxmox_ve_config::guest::{GuestEntry, GuestMap};
 use proxmox_ve_config::host::types::BridgeName;
@@ -257,13 +259,28 @@ impl NftConfigLoader for PveNftConfigLoader {
     }
 }
 
+pub struct FirewallSdnConfig {
+    _config: SdnConfig,
+    ipsets: BTreeMap<String, Ipset>,
+}
+
+impl FirewallSdnConfig {
+    pub fn ipsets(&self) -> &BTreeMap<String, Ipset> {
+        &self.ipsets
+    }
+
+    pub fn ipset(&self, name: &str) -> Option<&Ipset> {
+        self.ipsets.get(name)
+    }
+}
+
 pub struct FirewallConfig {
     cluster_config: ClusterConfig,
     host_config: HostConfig,
     guest_config: BTreeMap<Vmid, GuestConfig>,
     bridge_config: BTreeMap<BridgeName, BridgeConfig>,
     nft_config: BTreeMap<String, ListChain>,
-    sdn_config: Option<SdnConfig>,
+    sdn_config: Option<FirewallSdnConfig>,
     ipam_config: Option<Ipam>,
     interface_mapping: AltnameMapping,
 }
@@ -325,11 +342,21 @@ impl FirewallConfig {
 
     pub fn parse_sdn(
         firewall_loader: &dyn FirewallConfigLoader,
-    ) -> Result<Option<SdnConfig>, Error> {
+    ) -> Result<Option<FirewallSdnConfig>, Error> {
         Ok(match firewall_loader.sdn_running_config()? {
             Some(data) => {
                 let running_config: RunningConfig = serde_json::from_reader(data)?;
-                Some(SdnConfig::try_from(running_config)?)
+                let config = SdnConfig::try_from(running_config)?;
+
+                let ipsets = config
+                    .ipsets(None)
+                    .map(|ipset| (ipset.name().name().to_string(), ipset))
+                    .collect();
+
+                Some(FirewallSdnConfig {
+                    _config: config,
+                    ipsets,
+                })
             }
             _ => None,
         })
@@ -415,7 +442,7 @@ impl FirewallConfig {
         &self.nft_config
     }
 
-    pub fn sdn(&self) -> Option<&SdnConfig> {
+    pub fn sdn(&self) -> Option<&FirewallSdnConfig> {
         self.sdn_config.as_ref()
     }
 
@@ -431,22 +458,54 @@ impl FirewallConfig {
         self.interface_mapping.get(iface_name).map(|x| x.as_str())
     }
 
-    pub fn alias(&self, name: &AliasName, vmid: Option<Vmid>) -> Option<&Alias> {
+    fn guest_alias(&self, name: &str, vmid: Vmid) -> Option<&Alias> {
+        if let Some(guest_config) = self.guests().get(&vmid) {
+            return guest_config.alias(name);
+        }
+
+        log::warn!("trying to get alias {name} for non-existing guest: #{vmid}");
+        None
+    }
+
+    pub fn alias(&self, name: &RuleAliasName, vmid: Option<Vmid>) -> Option<&Alias> {
         log::trace!("getting alias {name:?}");
 
-        match name.scope() {
-            AliasScope::Datacenter => self.cluster().alias(name.name()),
-            AliasScope::Guest => {
-                if let Some(vmid) = vmid {
-                    if let Some(entry) = self.guests().get(&vmid) {
-                        return entry.alias(name);
-                    }
-
-                    log::warn!("trying to get alias {name} for non-existing guest: #{vmid}");
+        match name {
+            RuleAliasName::Scoped(alias_name) => match alias_name.scope() {
+                AliasScope::Datacenter => self.cluster().alias(alias_name.name()),
+                AliasScope::Guest => {
+                    vmid.and_then(|vmid| self.guest_alias(alias_name.name(), vmid))
                 }
+            },
+            RuleAliasName::Legacy(legacy_alias_name) => vmid
+                .and_then(|vmid| self.guest_alias(legacy_alias_name.as_ref(), vmid))
+                .or_else(|| self.cluster().alias(legacy_alias_name.as_ref())),
+        }
+    }
 
-                None
-            }
+    fn guest_ipset(&self, name: &str, vmid: Vmid) -> Option<&Ipset> {
+        if let Some(guest_config) = self.guests().get(&vmid) {
+            return guest_config.ipset(name);
+        }
+
+        log::warn!("trying to get ipset {name} for non-existing guest: #{vmid}");
+        None
+    }
+
+    pub fn ipset(&self, name: &RuleIpsetName, vmid: Option<Vmid>) -> Option<&Ipset> {
+        log::trace!("getting ipset {name:?}");
+
+        match name {
+            RuleIpsetName::Scoped(ipset_name) => match ipset_name.scope() {
+                IpsetScope::Sdn => self.sdn()?.ipset(ipset_name.name()),
+                IpsetScope::Datacenter => self.cluster().ipset(ipset_name.name()),
+                IpsetScope::Guest => {
+                    vmid.and_then(|vmid| self.guest_ipset(ipset_name.name(), vmid))
+                }
+            },
+            RuleIpsetName::Legacy(legacy_ipset_name) => vmid
+                .and_then(|vmid| self.guest_ipset(legacy_ipset_name.as_ref(), vmid))
+                .or_else(|| self.cluster().ipset(legacy_ipset_name.as_ref())),
         }
     }
 }

proxmox-firewall/src/firewall.rs
@@ -196,7 +196,7 @@ impl Firewall {
 
             let management_ips = HostConfig::management_ips()?;
 
-            let mut ipset = Ipset::from_parts(IpsetScope::Datacenter, "management");
+            let mut ipset = Ipset::new(IpsetName::new(IpsetScope::Datacenter, "management"));
             ipset.reserve(management_ips.len());
 
             let entries = management_ips.into_iter().map(IpsetEntry::from);
@@ -229,13 +229,10 @@ impl Firewall {
         let guest_table = Self::guest_table();
 
         if let Some(sdn_config) = self.config.sdn() {
-            let ipsets = sdn_config
-                .ipsets(None)
-                .map(|ipset| (ipset.name().to_string(), ipset))
-                .collect();
+            let ipsets = sdn_config.ipsets();
 
-            self.create_ipsets(&mut commands, &ipsets, &cluster_host_table, None)?;
-            self.create_ipsets(&mut commands, &ipsets, &guest_table, None)?;
+            self.create_ipsets(&mut commands, ipsets, &cluster_host_table, None)?;
+            self.create_ipsets(&mut commands, ipsets, &guest_table, None)?;
         }
 
         if let Some(ipam_config) = self.config.ipam() {

proxmox-firewall/tests/snapshots/integration_tests__firewall.snap
@@ -1822,6 +1822,54 @@ expression: "firewall.full_host_fw().expect(\"firewall can be generated\")"
     },
     {
       "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "group-network1-in",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": {
+                  "prefix": {
+                    "addr": "172.16.100.0",
+                    "len": 24
+                  }
+                }
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": {
+                  "prefix": {
+                    "addr": "172.16.100.0",
+                    "len": 24
+                  }
+                }
+              }
+            },
+            {
+              "accept": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
         "chain": {
           "family": "inet",
           "table": "proxmox-firewall",
@@ -1890,6 +1938,82 @@ expression: "firewall.full_host_fw().expect(\"firewall can be generated\")"
           "table": "proxmox-firewall",
           "chain": "cluster-in",
           "expr": [
+            {
+              "accept": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "cluster-in",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-dc/network1"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-dc/network1-nomatch"
+              }
+            },
+            {
+              "accept": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "cluster-in",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-dc/network1"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-dc/network1-nomatch"
+              }
+            },
             {
               "accept": null
             }
@@ -3952,6 +4076,54 @@ expression: "firewall.full_host_fw().expect(\"firewall can be generated\")"
           "family": "bridge",
           "table": "proxmox-firewall-guests",
           "name": "group-network1-in"
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "bridge",
+          "table": "proxmox-firewall-guests",
+          "chain": "group-network1-in",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": {
+                  "prefix": {
+                    "addr": "172.16.100.0",
+                    "len": 24
+                  }
+                }
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": {
+                  "prefix": {
+                    "addr": "172.16.100.0",
+                    "len": 24
+                  }
+                }
+              }
+            },
+            {
+              "accept": null
+            }
+          ]
         }
       }
     },

proxmox-firewall/src/object.rs
@@ -13,7 +13,7 @@ use proxmox_nftables::{
 use proxmox_ve_config::{
     firewall::{
         ct_helper::CtHelperMacro,
-        types::{alias::AliasName, ipset::IpsetAddress, Alias, Ipset},
+        types::{alias::RuleAliasName, ipset::IpsetAddress, Alias, Ipset},
     },
     guest::types::Vmid,
 };
@@ -29,7 +29,7 @@ pub(crate) struct NftObjectEnv<'a, 'b> {
 }
 
 impl NftObjectEnv<'_, '_> {
-    pub(crate) fn alias(&self, name: &AliasName) -> Option<&Alias> {
+    pub(crate) fn alias(&self, name: &RuleAliasName) -> Option<&Alias> {
         self.firewall_config.alias(name, self.vmid)
     }
 }

proxmox-firewall/src/rule.rs
@@ -14,14 +14,14 @@ use proxmox_ve_config::{
         ct_helper::CtHelperMacro,
         fw_macros::{get_macro, FwMacro},
         types::{
-            alias::AliasName,
-            ipset::{Ipfilter, IpsetName},
+            alias::RuleAliasName,
+            ipset::{Ipfilter, IpsetName, RuleIpsetName},
             log::LogRateLimit,
             rule::{Direction, Kind, RuleGroup, Verdict as ConfigVerdict},
             rule_match::{
                 Icmp, Icmpv6, IpAddrMatch, IpMatch, Ports, Protocol, RuleMatch, Sctp, Tcp, Udp,
             },
-            Alias, Rule,
+            Alias, Ipset, Rule,
         },
     },
     guest::types::Vmid,
@@ -121,7 +121,11 @@ pub(crate) struct NftRuleEnv<'a> {
 }
 
 impl NftRuleEnv<'_> {
-    fn alias(&self, name: &AliasName) -> Option<&Alias> {
+    fn ipset(&self, name: &RuleIpsetName) -> Option<&Ipset> {
+        self.firewall_config.ipset(name, self.vmid)
+    }
+
+    fn alias(&self, name: &RuleAliasName) -> Option<&Alias> {
         self.firewall_config.alias(name, self.vmid)
     }
 
@@ -289,10 +293,14 @@ impl ToNftRules for RuleMatch {
 /// `name`. It matches the IPs contained in the ipset with the field given in `field_name`.
 fn handle_set(
     rules: &mut Vec<NftRule>,
-    name: &IpsetName,
+    name: &RuleIpsetName,
     field_name: &str,
     env: &NftRuleEnv,
 ) -> Result<(), Error> {
+    let Some(ipset) = env.ipset(name) else {
+        bail!("could not find ipset {name}");
+    };
+
     let mut new_rules = rules
         .drain(..)
         .flat_map(|rule| {
@@ -310,7 +318,7 @@ fn handle_set(
                         field.clone(),
                         Expression::set_name(&SetName::ipset_name(
                             Family::V4,
-                            name,
+                            ipset.name(),
                             env.vmid,
                             false,
                         )),
@@ -321,7 +329,7 @@ fn handle_set(
                         field,
                         Expression::set_name(&SetName::ipset_name(
                             Family::V4,
-                            name,
+                            ipset.name(),
                             env.vmid,
                             true,
                         )),
@@ -344,7 +352,7 @@ fn handle_set(
                         field.clone(),
                         Expression::set_name(&SetName::ipset_name(
                             Family::V6,
-                            name,
+                            ipset.name(),
                             env.vmid,
                             false,
                         )),
@@ -355,7 +363,7 @@ fn handle_set(
                         field,
                         Expression::set_name(&SetName::ipset_name(
                             Family::V6,
-                            name,
+                            ipset.name(),
                             env.vmid,
                             true,
                         )),

proxmox-firewall/tests/input/cluster.fw
@@ -20,8 +20,10 @@ dc/network1
 
 GROUP network1 -i eth0
 IN ACCEPT -log nolog
+IN ACCEPT -dest +network1
 
 [group network1]
 
 IN ACCEPT -source dc/network1 -dest dc/network1 -log nolog
+IN ACCEPT -source network1 -dest network1 -log nolog
 

commit e5f75702c8c6753849f375b71c6b93a6cbeb55a2

firewall: fix ipset lookup for auto-generated ipam ipsets
In commit 1a917517 the ipset lookup function was adapted such that it
can support both legacy ipset names as well as the new format that
includes scopes. During this change, the function that performs the
lookup for the SDN scope only included ipsets from the SDN
configuration, but not the IPAM configuration. While the IPAM ipsets
were correctly generated, the firewall refused to generate rules that
included the IPAM ipsets, since they were not found in the lookup
function. Adapt the lookup function to consider the IPAM ipsets as
well to fix this issue.

Also add respective rules to the integration tests, which would have
caught this issue.

Reported in the forum by a user [1]

[1] https://forum.proxmox.com/threads/sdn-aliases-not-found-by-firewall.180549/

Fixes: 1a917517
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20260212084832.63278-3-s.hanreich@proxmox.com

proxmox-firewall/src/config.rs
@@ -274,6 +274,21 @@ impl FirewallSdnConfig {
     }
 }
 
+pub struct FirewallIpamConfig {
+    _config: Ipam,
+    ipsets: BTreeMap<String, Ipset>,
+}
+
+impl FirewallIpamConfig {
+    pub fn ipsets(&self) -> &BTreeMap<String, Ipset> {
+        &self.ipsets
+    }
+
+    pub fn ipset(&self, name: &str) -> Option<&Ipset> {
+        self.ipsets.get(name)
+    }
+}
+
 pub struct FirewallConfig {
     cluster_config: ClusterConfig,
     host_config: HostConfig,
@@ -281,7 +296,7 @@ pub struct FirewallConfig {
     bridge_config: BTreeMap<BridgeName, BridgeConfig>,
     nft_config: BTreeMap<String, ListChain>,
     sdn_config: Option<FirewallSdnConfig>,
-    ipam_config: Option<Ipam>,
+    ipam_config: Option<FirewallIpamConfig>,
     interface_mapping: AltnameMapping,
 }
 
@@ -362,11 +377,23 @@ impl FirewallConfig {
         })
     }
 
-    pub fn parse_ipam(firewall_loader: &dyn FirewallConfigLoader) -> Result<Option<Ipam>, Error> {
+    pub fn parse_ipam(
+        firewall_loader: &dyn FirewallConfigLoader,
+    ) -> Result<Option<FirewallIpamConfig>, Error> {
         Ok(match firewall_loader.ipam()? {
             Some(data) => {
                 let raw_ipam: IpamJson = serde_json::from_reader(data)?;
-                Some(Ipam::try_from(raw_ipam)?)
+                let ipam = Ipam::try_from(raw_ipam)?;
+
+                let ipsets = ipam
+                    .ipsets(None)
+                    .map(|ipset| (ipset.name().name().to_string(), ipset))
+                    .collect();
+
+                Some(FirewallIpamConfig {
+                    _config: ipam,
+                    ipsets,
+                })
             }
             _ => None,
         })
@@ -446,7 +473,7 @@ impl FirewallConfig {
         self.sdn_config.as_ref()
     }
 
-    pub fn ipam(&self) -> Option<&Ipam> {
+    pub fn ipam(&self) -> Option<&FirewallIpamConfig> {
         self.ipam_config.as_ref()
     }
 
@@ -497,7 +524,10 @@ impl FirewallConfig {
 
         match name {
             RuleIpsetName::Scoped(ipset_name) => match ipset_name.scope() {
-                IpsetScope::Sdn => self.sdn()?.ipset(ipset_name.name()),
+                IpsetScope::Sdn => self
+                    .sdn()?
+                    .ipset(ipset_name.name())
+                    .or_else(|| self.ipam()?.ipset(ipset_name.name())),
                 IpsetScope::Datacenter => self.cluster().ipset(ipset_name.name()),
                 IpsetScope::Guest => {
                     vmid.and_then(|vmid| self.guest_ipset(ipset_name.name(), vmid))

proxmox-firewall/src/firewall.rs
@@ -249,10 +249,7 @@ impl Firewall {
         }
 
         if let Some(ipam_config) = self.config.ipam() {
-            let ipsets = ipam_config
-                .ipsets(None)
-                .map(|ipset| (ipset.name().to_string(), ipset))
-                .collect();
+            let ipsets = ipam_config.ipsets();
 
             self.create_ipsets(&mut commands, &ipsets, &cluster_host_table, None)?;
             self.create_ipsets(&mut commands, &ipsets, &guest_table, None)?;

proxmox-firewall/tests/input/host.fw
@@ -24,4 +24,6 @@ IN ACCEPT --icmp-type neighbor-solicitation --proto ipv6-icmp --log info
 IN Ping(REJECT)
 IN REJECT -p udp --dport 443
 OUT REJECT -p udp --dport 443
+FORWARD DROP --source +sdn/guest-ipam-101 --dest +sdn/guest-ipam-101
+FORWARD DROP --source +sdn/public-all --dest +sdn/public-gateway
 

proxmox-firewall/tests/snapshots/integration_tests__firewall.snap
@@ -1,8 +1,6 @@
 ---
 source: proxmox-firewall/tests/integration_tests.rs
-assertion_line: 127
 expression: "firewall.full_host_fw().expect(\"firewall can be generated\")"
-snapshot_kind: text
 ---
 {
   "nftables": [
@@ -4068,6 +4066,254 @@ snapshot_kind: text
               "jump": {
                 "target": "do-reject"
               }
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "host-forward",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v4-sdn/guest-ipam-101"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v4-sdn/guest-ipam-101-nomatch"
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-sdn/guest-ipam-101"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-sdn/guest-ipam-101-nomatch"
+              }
+            },
+            {
+              "drop": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "host-forward",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v6-sdn/guest-ipam-101"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v6-sdn/guest-ipam-101-nomatch"
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-sdn/guest-ipam-101"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-sdn/guest-ipam-101-nomatch"
+              }
+            },
+            {
+              "drop": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "host-forward",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v4-sdn/public-all"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v4-sdn/public-all-nomatch"
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-sdn/public-gateway"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v4-sdn/public-gateway-nomatch"
+              }
+            },
+            {
+              "drop": null
+            }
+          ]
+        }
+      }
+    },
+    {
+      "add": {
+        "rule": {
+          "family": "inet",
+          "table": "proxmox-firewall",
+          "chain": "host-forward",
+          "expr": [
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v6-sdn/public-all"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "saddr"
+                  }
+                },
+                "right": "@v6-sdn/public-all-nomatch"
+              }
+            },
+            {
+              "match": {
+                "op": "==",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-sdn/public-gateway"
+              }
+            },
+            {
+              "match": {
+                "op": "!=",
+                "left": {
+                  "payload": {
+                    "protocol": "ip6",
+                    "field": "daddr"
+                  }
+                },
+                "right": "@v6-sdn/public-gateway-nomatch"
+              }
+            },
+            {
+              "drop": null
             }
           ]
         }